f2c18b194bf3d77da6300ebf35e5fc45344c9c07
rajguru0123 <147732976+rajguru0123@users.noreply.github.com>
2026-02-26 18:12:49 +0000

    var-conversion

---
+++ test.js
@@ -0,0 +1,37 @@
+const accountId=1234;
+let EmailId="nitishkumar@gmail.com";
+accountCity="Bangalore";
+// var accountPass="12345"; prefere not use var because of issue in block scope and function scope
+// accountId=33;  not allowed
+EmailId="gmail@gmail.com"
+accountCity="lucknow"
+
+
+
+
+let loggedin=false;//boolean true/false
+let name="nitish";
+ age =23;
+// null => standalone value that represents the absence of any object value result come out object.
+let phoneNo=null;  //null is a object type.
+// undefined => variable that has been declared but not assigned a value result cone out undefined.
+let address=undefined;  //undefined is a type of.
+// Symbol =unique;
+// console.log(typeof phoneNo);
+
+
+
+/* conversion string to number, number to string,
+ boolean to number, number to boolean, string to boolean, boolean to string. */
+// let score="110abc";
+// let score=true;// false=>0, true=>1.
+// let valueInNumber=Number(score);
+// console.log(score);
+// console.log(typeof (score));
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let score=0;// 0=>false, 1=>true.
+let valueInBoolean=Boolean(score);
+console.log(valueInBoolean);
+
